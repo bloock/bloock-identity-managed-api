2268eabda3039ab6d9d9b8bb797cf7d6684a12a7
edutomesco <eduard.escoruela@gmail.com>
2023-12-29 16:26:41 +0100

    EIT-3468 | add image as form data to upload an image

--- internal/platform/server/handler/issuer/create_issuer.go
+++ internal/platform/server/handler/issuer/create_issuer.go
@@ -4,24 +4,28 @@ import (
 	api_error "bloock-identity-managed-api/internal/platform/server/error"
 	"bloock-identity-managed-api/internal/services/create"
 	"bloock-identity-managed-api/internal/services/create/request"
+	"encoding/base64"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog"
+	"io"
+	"mime/multipart"
 	"net/http"
 )
 
 type CreateIssuerRequest struct {
-	Key             string      `json:"key" binding:"required"`
-	DidMetadata     DidMetadata `json:"did_metadata"`
-	Name            string      `json:"name"`
-	Description     string      `json:"description"`
-	Image           string      `json:"image"`
-	PublishInterval int64       `json:"publish_interval"`
+	Key             string `form:"key" binding:"required"`
+	DidMetadata     DidMetadata
+	Name            string                `form:"name"`
+	Description     string                `form:"description"`
+	Image           *multipart.FileHeader `form:"image"`
+	PublishInterval int64                 `form:"publish_interval"`
 }
 
 type DidMetadata struct {
-	Method     string `json:"method"`
-	Blockchain string `json:"blockchain"`
-	Network    string `json:"network"`
+	Method     string `form:"did_metadata.method"`
+	Blockchain string `form:"did_metadata.blockchain"`
+	Network    string `form:"did_metadata.network"`
 }
 
 type CreateIssuerResponse struct {
@@ -37,9 +41,20 @@ func mapToCreateIssuerResponse(did string) CreateIssuerResponse {
 func CreateIssuer(l zerolog.Logger) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		var req CreateIssuerRequest
-		if err := ctx.ShouldBind(&req); err != nil {
+		if err := ctx.Bind(&req); err != nil {
 			ctx.JSON(http.StatusBadRequest, api_error.NewBadRequestAPIError(err.Error()))
 			return
+		}
+
+		var image []byte
+		var err error
+		if req.Image != nil {
+			image, err = loadImage(req.Image)
+			if err != nil {
+				badRequestAPIError := api_error.NewBadRequestAPIError(err.Error())
+				ctx.JSON(badRequestAPIError.Status, badRequestAPIError)
+				return
+			}
 		}
 
 		createIssuerService := create.NewIssuer(ctx, req.Key, l)
@@ -53,7 +68,7 @@ func CreateIssuer(l zerolog.Logger) gin.HandlerFunc {
 			},
 			Name:            req.Name,
 			Description:     req.Description,
-			Image:           req.Image,
+			Image:           base64.URLEncoding.EncodeToString(image),
 			PublishInterval: req.PublishInterval,
 		}
 
@@ -66,4 +81,21 @@ func CreateIssuer(l zerolog.Logger) gin.HandlerFunc {
 
 		ctx.JSON(http.StatusCreated, mapToCreateIssuerResponse(issuerDid))
 	}
+}
+
+func loadImage(image *multipart.FileHeader) ([]byte, error) {
+	imageReader, err := image.Open()
+	if err != nil {
+		return nil, err
+	}
+
+	file, err := io.ReadAll(imageReader)
+	if err != nil {
+		return nil, err
+	}
+	if len(file) == 0 {
+		return nil, fmt.Errorf("file must be a valid file")
+	}
+
+	return file, nil
 }
